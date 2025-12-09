1d02bc4655f2127565e758f5145d4a1e7e0d525b
lcann <liamcannon93@gmail.com>
2025-12-09 19:20:28 +0000

    Add dotenv configuration and database connection setup

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // Imports
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
-
 const path = require('path');
+const mysql = require('mysql2');
 
 // Create the express application object
 const app = express();
 const port = 8000;
 
+
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
 
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true }));
+
 // Set up public folder (for css and static js)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the database connection pool
+const db = mysql.createPool({
+    host: process.env.HEALTH_HOST,
+    user: process.env.HEALTH_USER,
+    password: process.env.HEALTH_PASSWORD,
+    database: process.env.HEALTH_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require('./routes/main');

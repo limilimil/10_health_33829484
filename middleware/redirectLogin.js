caba24bde0c6bee515b817911188a58846548ae0
lcann <liamcannon93@gmail.com>
2025-12-14 20:48:16 +0000

    Add redirect override for development environment

--- middleware/redirectLogin.js
+++ middleware/redirectLogin.js
@@ -1,5 +1,9 @@
 const redirectLogin = ({ sessionID, redirectPath }) => {
     return (req, res, next) => {
+        // Skip redirect in development environment
+        if (process.env.HEALTH_NODE_ENV === 'development') {
+            return next();
+        }
         if (!req.session[sessionID]) {
             res.redirect(process.env.HEALTH_BASE_PATH + redirectPath); // redirect to the login page
         } else { 

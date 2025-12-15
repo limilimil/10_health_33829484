327e6d9bb795866b6e4795c1ad0ea2f5ca6f6570
lcann <liamcannon93@gmail.com>
2025-12-15 22:42:41 +0000

    Add error handling and validation feedback to patient registration form

--- routes/patients.js
+++ routes/patients.js
@@ -1,6 +1,8 @@
 // Create a new router
 const express = require('express');
 const router = express.Router();
+const { check, validationResult, matchedData } = require('express-validator');
+
 const bcrypt = require('bcrypt');
 
 const patientsModel = require('../models/patientsModel');
@@ -10,6 +12,26 @@ const redirectLogin = require('../middleware/redirectLogin');
 const patientsRedirect = redirectLogin({ sessionID: 'userID', redirectPath: '/patients/login' });
 
 const saltRounds = 10;
+
+function validatekNHSNumber(nhsNumber) {
+    const digits = nhsNumber.replace(/\s+/g, '');
+    const numbers = digits.split('').map(Number);
+    if (numbers.length !== 10) {
+        return false;
+    }
+
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += numbers[i] * (10 - i);
+    }
+
+    const remainder = sum % 11;
+    const checkDigit = remainder === 0 ? 0 : 11 - remainder;
+
+    if (checkDigit === 10) return false;
+
+    return checkDigit === numbers[9];
+}
 
 // Route handlers
 
@@ -60,20 +82,45 @@ router.get('/logout', patientsRedirect, (req, res) => {
 });
 
 router.get('/register', (req, res, next) => {
-    res.render('register.ejs', { title: 'Patient registration form'});
+    res.render('register.ejs', { title: 'Patient registration form', errors: {}, formData: {} });
 });
 
-router.post('/registered', async (req, res, next) => {
-    const plainPassword = req.body.password;
-    try {
-        const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
-        const newUser = [req.body.first, req.body.last, req.body.email, req.body.nhs, req.body.username, hashedPassword];
-        const result = await patientsModel.insert(newUser);
-        res.send("Patient registered");
-    } catch(err) {
-        console.error(err);
-        next(err);
-    }
+router.post('/registered', 
+    [
+        check('first').trim().isLength({ min: 1, max: 50 }).withMessage("First name cannot be over 50 characters").isAlpha().withMessage("Please enter letters only"),
+        check('last').trim().isLength({ min: 1, max: 50 }).withMessage("Last name cannot be over 50 characters").isAlpha().withMessage("Please enter letters only"),
+        check('email').isEmail().withMessage("Please enter a valid email address").trim().isLength({ min: 5, max: 120 }).withMessage("Email address invalid"), 
+        check('nhs').custom(validatekNHSNumber).withMessage("The NHS number you entered cannot be verified").customSanitizer(nhs => nhs.replace(/\s+/g, '')),
+        check('username').trim().isLength({ min: 4, max: 30}).withMessage("Username must be between 4 and 30 characters").isAlphanumeric().withMessage("Username must be letters and numbers only"),
+        check('password').isLength({ min: 8, max: 64}).withMessage("Password must be between 8 and 64 characters")
+
+    ], async (req, res, next) => {
+        const errors = validationResult(req);
+        const data = matchedData(req);
+        if (!errors.isEmpty()) {
+            // Retrieves relevant error messages to be displayed on the user interface
+            const errorMessages = { first: [], last: [], email: [], nhs: [], username: [], password: [] };
+            for (let i = 0; i < errors.errors.length; i++) {
+                let inputField = errors.errors[i].path
+                if(errorMessages[inputField]) { // Only includes error messages for existing input types
+                    errorMessages[inputField].push(errors.errors[i].msg);
+                }
+            }
+            // Loads the registration page again with error messages and previous inputs except password
+            res.render('./register', { title: "Patient registration form", errors: errorMessages, formData: data });
+        } else {
+            const plainPassword = data.password;
+            try {
+                const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+                const newUser = [data.first, data.last, data.email, data.nhs, data.username, hashedPassword];
+                const result = await patientsModel.insert(newUser);
+                res.send("Patient registered");
+            } catch(err) {
+                console.error(err);
+                next(err);
+            }
+        }
+
 });
 
 // Export the router object so index.js can access it
